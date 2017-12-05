129d8ec2aceda93e2ac4ca96df0a9e4cc7c9ae47
EricH77410 <rico@club-internet.fr>
2017-12-05 20:25:54 +0100

    Added FullView

--- src/components/recipes/FullDisplay.js
+++ src/components/recipes/FullDisplay.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
 const FullDisplay = (props) => {
+    const ingredients = props.recette.ingredients.split(',').map((item, key) => <li key={key}>{item}</li>);
+    const instructions = props.recette.instructions.split('\n').map((item, key) => <li key={key}>{item}</li>);
+    
     return (
-        <div>
-            Full display
+        <div className="content-container">
+                <div className="card">
+            <div className="image">
+                <img src={props.recette.image} alt="" />
+            </div>
+            <div className="recette">
+                <h2>{props.recette.nom}</h2>
+                <h4>{props.recette.description}</h4>
+                <p>
+                    <span className="glyphicon glyphicon-time time"></span> {props.recette.tempsPreparation}
+                    <span className="glyphicon glyphicon-fire cuisson"></span> {props.recette.tempsCuisson}
+                    <span className="glyphicon glyphicon-education dificulty"></span> {props.recette.difficulte}
+                </p>
+                <ul className="liste-ingredients">
+                    {ingredients}
+                </ul>
+                <ol className="liste-instruction">
+                   {instructions}
+                </ol>
+                
+            </div>
         </div>
+        </div>
+
     )
 }
 
-export default FullDisplay;
+const mapStateToProps = (state,props) => {
+     return {
+         recette: state.recipes.find((r)=>r.id === props.match.params.id)
+     }
+ }
+
+export default connect(mapStateToProps)(FullDisplay);
